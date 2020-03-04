3cb6729bb5ff71511bebe4de55cb9020f1228d6f
Heejeong Cho <w0424547@nscc.ca>
2020-03-04 19:53:31 -0400

    Populate journal page with some content

---
+++ src/pages/journal-content.js
@@ -0,0 +1,34 @@
+const journals = [
+    {
+        date: '2020-03-04',
+        title: 'New opportunity!',
+        content: [
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Cras pulvinar mattis nunc sed blandit. 
+            Risus at ultrices mi tempus imperdiet nulla malesuada. 
+            Gravida arcu ac tortor dignissim convallis. Habitant morbi 
+            tristique senectus et netus et malesuada fames ac. 
+            Sit amet facilisis magna etiam tempor. 
+            Phasellus faucibus scelerisque eleifend donec pretium vulputate 
+            sapien nec sagittis. Enim nec dui nunc mattis enim ut. 
+            Neque sodales ut etiam sit.`,
+            `Adipiscing elit ut aliquam purus sit amet luctus venenatis. 
+            Integer malesuada nunc vel risus. Amet aliquam id diam maecenas 
+            ultricies mi eget mauris. Mauris cursus mattis molestie a iaculis. 
+            Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. 
+            Urna duis convallis convallis tellus id. Morbi blandit cursus risus 
+            at ultrices. Purus ut faucibus pulvinar elementum integer enim neque 
+            volutpat ac. Vulputate ut pharetra sit amet aliquam id diam. `,
+            `Habitasse platea dictumst quisque sagittis purus sit. 
+            Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.
+            Urna porttitor rhoncus dolor purus non. Gravida arcu ac tortor dignissim convallis aenean. 
+            Iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Aliquet enim tortor 
+            at auctor urna. Ante in nibh mauris cursus. 
+            Netus et malesuada fames ac turpis egestas integer eget aliquet. T
+            empor commodo ullamcorper a lacus vestibulum sed arcu.`
+        ]
+    }
+];
+
+export default journals;
--- src/pages/journal.js
+++ src/pages/journal.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import journalContent from './journal-content';
 
-const JournalPage = () => (
-    <>
-        <h1>My Journal</h1>
-        
-    </>    
-);
+const JournalPage = ({ match }) => {
+    const date = match.params.date;
+    const journal = journalContent.find(journal => journal.date === date); 
+
+    if (!journal) return <h3>Your journal does not exist :(</h3>
+
+    return(
+        <>
+            <h1>{journal.title}</h1>      
+            <h4>{journal.date}</h4>  
+            <br/>
+            {journal.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+                
+        </>    
+    );
+};
 
 export default JournalPage;
