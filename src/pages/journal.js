0b24900cd8fc1cdb2c8c70c5778ba6cf2a81465c
Heejeong Cho <w0424547@nscc.ca>
2020-03-04 20:20:55 -0400

    include other journals

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import journalContent from './journal-content';
+import JournalList from '../components/journalList';
+import JournalContent from './journal-content';
 
 const JournalPage = ({ match }) => {
     const date = match.params.date;
-    const journal = journalContent.find(journal => journal.date === date); 
+    const journal = JournalContent.find(journal => journal.date === date); 
 
     if (!journal) return <h3>Your journal does not exist :(</h3>
+
+    const otherJournals = JournalContent.filter(journal => journal.date !== date);
 
     return(
         <>
             <h1>{journal.title}</h1>      
             <h4>{journal.date}</h4>  
-            <br/>
             {journal.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <br />
+            <br />
+            <h3>Other Journals</h3>
+            <JournalList journals={otherJournals} />
                 
         </>    
     );
